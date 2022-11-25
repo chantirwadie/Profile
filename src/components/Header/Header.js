e6edfa99b4ab43e0d220726eac6e37ef710fedbb
chantirwadie <chantirwadie@gmail.com>
2022-11-25 20:02:38 +0100

    changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () => (
     <Div1>
       <Link href="/">
         <a style={{ display: "flex", alignItems: "center", color: "white" ,marginBottom:"20px",}}>
-          <CgProfile style={{marginRight:"5px"}} size="2rem" /> <Span>CHANTIR Wadie</Span>
+          <Span>CHANTIR Wadie</Span>
         </a>
       </Link>
     </Div1>
